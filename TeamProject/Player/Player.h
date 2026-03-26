bbc2e623e513d5ae3f996bf2e1ce8992ee174104
rubin3150 <rubin3150@naver.com>
2026-03-26 17:03:55 +0900

    feat: Player, Warrior
    - 플레이어 기본 틀 생성

--- TeamProject/Player/Player.h
+++ TeamProject/Player/Player.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <string>
 #include <vector>
 using namespace std;
@@ -6,38 +6,57 @@ using namespace std;
 struct Map;
 struct Monster;
 
-// ── 플레이어 ──────────────────────────────────────────
-struct Player
+class Player
 {
+protected:
     // 위치
     int x = 0, y = 0;
 
-    // 기본 정보
-    string name;
+    // status
+    string job;
+    int hp;
+    int maxHp;
+    int atk;
+    int dex;
 
-    // 전투 스탯
-    // text-rpg: defaultHealth = baseHp + constitution*10
-    int hp        = 200;
-    int maxHp     = 200;
-    int atk       = 30;
-    int def       = 0;
+    int level;
+    int exp;
+    int expToNext;
+    //int gold      = 0;
 
-    // 성장 (text-rpg experience 시스템 참고)
-    int level     = 1;
-    int exp       = 0;
-    int expToNext = 100;
-    int gold      = 0;
-
-    // 인벤토리 & 상태이상
     // Inventory            inventory;
     // vector<StatusEffect> statusEffects;
 
-    // ── 행동 ────────────────────────────────────────
-    void tryMove(int dx, int dy, Map& map, vector<Monster>& monsters);
-    void gainExp(int amount);
-    void levelUp();
-    void tickStatus();
+public:
+    Player();
 
-    bool isAlive() const { return hp > 0; }
-    void printStatus() const;
+    virtual ~Player();
+
+    // 나중에 2칸씩 움직이는 캐릭터도 있을 수 있으니 일단 가상함수로 구현
+    virtual void TryMove(int _dx, int _dy, Map& _map) = 0;
+
+    //virtual void attack() = 0;
+    //virtual void attack(Monster* monster) = 0;
+
+    // Getter
+    string GetJobName();
+    int GetLevel();
+    int GetHP();
+    int GetAtk();
+    int GetDex();
+
+    // Setter
+    void SetLevel(int level);
+    void SetHp(int hp);
+    void SetAtk(int atk);
+    void SetDex(int dex);
+
+    // player info
+    bool IsAlive() const { return hp > 0; }
+    void PrintStatus() const;
+
+    void GainExp(int _exp);
+    void LevelUp();
+    void TakeDamage(int _damage);
+    bool Die();
 };
