9b7567a46a18cdfebb933863d907a4dc7ead0023
NaReun73 <rnlcksg37@gmail.com>
2026-03-26 14:52:39 +0900

    feat: 몬스터 클래스 정의
    기본

--- TeamProject/Monster/Monster.cpp
+++ TeamProject/Monster/Monster.cpp
@@ -1,1 +1,52 @@
 ﻿#include "Monster.h"
+#include <iostream>
+
+Monster::Monster(std::string name, int hp, int power, int speed)
+    :M_name(name), M_hp(hp), M_power(power), M_speed(speed)
+{
+}
+
+std::string Monster::GetName()
+{
+    return M_name;
+}
+
+int Monster::GetHP()
+{
+    return M_hp;
+}
+
+int Monster::GetPower()
+{
+    return M_power;
+}
+
+int Monster::GetSpeed()
+{
+    return M_speed;
+}
+
+void Monster::SetName(std::string name)
+{
+    M_name = name;
+}
+
+void Monster::SetHP(int hp)
+{
+    M_hp = hp;
+}
+
+void Monster::SetPower(int power)
+{
+    M_power = power;
+}
+
+void Monster::SetSpeed(int speed)
+{
+    M_speed = speed;
+}
+
+void Monster::TakeDamage(int damage)
+{
+    M_hp -= damage;
+}
