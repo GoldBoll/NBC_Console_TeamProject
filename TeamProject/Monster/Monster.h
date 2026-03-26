44e593b380792644abaec58835098629f95924a9
NaReun73 <rnlcksg37@gmail.com>
2026-03-26 14:53:27 +0900

    feat: 몬스터 클래스 헤더파일
    헤더파일 추가

--- TeamProject/Monster/Monster.h
+++ TeamProject/Monster/Monster.h
@@ -1,7 +1,26 @@
 ﻿#pragma once
+#include <string>
 
 class Monster
 {
+protected:
+    std::string M_name;
+    int M_hp;
+    int M_power;
+    int M_speed;
+
 public:
-    
+    Monster(std::string name, int hp, int power, int speed);
+
+    std::string GetName();
+    int GetHP();
+    int GetPower();
+    int GetSpeed();
+
+    void SetName(std::string name);
+    void SetHP(int hp);
+    void SetPower(int power);
+    void SetSpeed(int speed);
+
+    void TakeDamage(int damage);
 };
