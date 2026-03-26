6e74031001deb232d23137a56380749d50757dd8
NaReun73 <rnlcksg37@gmail.com>
2026-03-26 17:26:07 +0900

    feat: SpawnManager
    -스폰매니저 생성

---
+++ TeamProject/SpawnManager.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Map/Map.h"
+#include "Monster/Monster.h"
+#include "Goblin.h"
+#include "Orc.h"
+#include "Mimic.h"
+#include "Golem.h"
+#include <vector>
+
+class SpawnManager
+{
+private:
+    SpawnManager();
+    ~SpawnManager();
+
+    std::vector<Monster*> activeMonsters;
+
+public:
+    static SpawnManager* GetInstance()
+    {
+        static SpawnManager instance;
+        return &instance;
+    }
+
+    void SpawnMonster(int _mapNumber, Monster* _pmonster, int _targetX, int _targetY);
+};
+
